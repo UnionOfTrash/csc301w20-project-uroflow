a211621952d6551067de4639829f06f862f36bd7
KarenChen <kareny.chen@mail.utoronto.ca>
2020-03-05 16:55:35 -0500

    d2: fixed date range picker border

--- src/webapp/src/MainPanel/RecordList.jsx
+++ src/webapp/src/MainPanel/RecordList.jsx
@@ -47,8 +47,7 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     width: 'auto',
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
+    borderRadius: '2%',
     padding: theme.spacing(2, 4, 3),
   },
   modalheader: {
