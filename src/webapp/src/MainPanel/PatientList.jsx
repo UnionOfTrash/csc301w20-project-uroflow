456397c388a226ce2dd575868ec71d2dcb8d4ac1
kexinlin <kexin.lin@mail.utoronto.ca>
2020-03-06 17:24:56 -0500

    add multiline property for notes in add-patients panel and align fields in patient table

--- src/webapp/src/MainPanel/PatientList.jsx
+++ src/webapp/src/MainPanel/PatientList.jsx
@@ -1,76 +1,94 @@
-import React from 'react';
-import { Button, ButtonGroup } from '@material-ui/core';
-import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell} from '@material-ui/core';
-import { FiberManualRecord } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
-
+import React from "react";
+import { Button, ButtonGroup } from "@material-ui/core";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
+} from "@material-ui/core";
+import { FiberManualRecord } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   container: {
-    overflowY: 'auto'
+    overflowY: "auto"
   },
   head: {
     padding: theme.spacing(1),
-    textAlign: 'center',
+    textAlign: "center"
   },
   grid: {
     margin: theme.spacing(0.5),
-    alignItems: 'center',
+    alignItems: "center"
   },
   form: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center"
   },
   input: {
     marginLeft: theme.spacing(1),
-    flex: 1,
+    flex: 1
   },
   iconButton: {
-    padding: 10,
+    padding: 10
   },
   sort: {
-    padding: theme.spacing(1),
+    padding: theme.spacing(1)
   }
 }));
 
 function PatientList(props) {
-
-  const rows = props.patients
+  const rows = props.patients;
 
   const classes = useStyles();
 
   return (
-      <TableContainer className={ classes.container } >
-        <Table stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell>StudyId</TableCell>
-              <TableCell align="right">RecNum</TableCell>
-              <TableCell align="right">New</TableCell>
-              <TableCell align="right">Detail</TableCell>
+    <TableContainer className={classes.container}>
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            <TableCell>StudyId</TableCell>
+            <TableCell align="center">RecNum</TableCell>
+            <TableCell align="center">New</TableCell>
+            <TableCell align="center">Detail</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow hover key={row.studyId}>
+              <TableCell component="th" scope="row">
+                {" "}
+                {row.studyId}{" "}
+              </TableCell>
+              <TableCell align="center"> {row.numRecord} </TableCell>
+              <TableCell align="center">
+                {" "}
+                <FiberManualRecord
+                  onClick={() => alert("click !")}
+                  fontSize="small"
+                  color={row.hasNew ? "primary" : "disabled"}
+                />{" "}
+              </TableCell>
+              <TableCell align="center">
+                <ButtonGroup variant="text">
+                  <Button
+                    onClick={() => props.onDetailClick(row.studyId)}
+                    color="secondary"
+                  >
+                    {" "}
+                    Details{" "}
+                  </Button>
+                </ButtonGroup>
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {
-              rows.map(row => (
-                <TableRow hover key={ row.studyId } >
-                  <TableCell component='th' scope='row'> { row.studyId } </TableCell>
-                  <TableCell align='left'> { row.numRecord } </TableCell>
-                  <TableCell align='left'> <FiberManualRecord onClick={() => alert("click !")} fontSize='small' color={ row.hasNew ? 'primary' : 'disabled' } /> </TableCell>
-                  <TableCell align='right'>
-                    <ButtonGroup variant='text'>
-                      <Button onClick={() => props.onDetailClick(row.studyId)} color='secondary'> Details </Button>
-                    </ButtonGroup>
-                  </TableCell>
-                </TableRow>
-              ))
-            }
-          </TableBody>
-        </Table>
-      </TableContainer>
-    
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
-export {PatientList};
+export { PatientList };
