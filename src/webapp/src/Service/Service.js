7d7a84a9d8c1dd6ed84ea62a1b1bf55f84a21e10
Qiushi Shi <ed.shi@mail.utoronto.ca>
2020-03-06 16:30:52 -0500

    update fake record data

--- src/webapp/src/Service/Service.js
+++ src/webapp/src/Service/Service.js
@@ -34,62 +34,6 @@ function getRecords(id){
         
     })
 }
-function getCurveData(cid){
-
-    return new Promise((res,rej) => {
-        setTimeout(() => {
-            const data = [
-                [
-                    0, 
-                    1, 
-                    2, 
-                    3, 
-                    4, 
-                    5, 
-                    6, 
-                    7, 
-                    8, 
-                    9, 
-                    10, 
-                    11, 
-                    12, 
-                    13, 
-                    14, 
-                    15, 
-                    16, 
-                    17, 
-                    18, 
-                    19, 
-                    20
-                ],
-                [
-                    0, 
-                    0.13, 
-                    0.13, 
-                    8.26, 
-                    14.09, 
-                    14.95, 
-                    17.23, 
-                    11.85, 
-                    2.10, 
-                    13.76, 
-                    42.70, 
-                    20.78, 
-                    21.89, 
-                    13.76, 
-                    1.74, 
-                    20.58, 
-                    23.45, 
-                    9.27, 
-                    0.49, 
-                    0, 
-                    0
-                ]
-            ]
-            res(data)
-        }, 1500)
-    })
-}
 
 function addPatient(data){
     //console.log(data)
